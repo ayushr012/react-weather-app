bc4390e15bf09ba6973381d0bd824cde4a168940
Vanshak1301 <vanshak.vats1301@gmail.com>
2025-04-22 13:23:12 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import './App.css';
+import React, { useState,useEffect } from "react";
+import Navbar from "../src/components/navbar";
+import MainWeather from "../src/components/MainWeather";
+import FiveDayForecast from "../src/components/fiveday";
+import TodayHighlights from "../src/components/todayhighlights";
+
+import axios from "axios";
+
+const WeatherDashboard = () => {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('Delhi'); // Default city is set to London
+  const [airQualityData, setAirQualityData] = useState(null);
+  const [fiveDayForecast, setFiveDayForecast] = useState(null);
+
+  useEffect(() => {
+    fetchWeatherData(city);
+  }, [city]);
+
+  const fetchAirQualityData = (lat, lon) => {
+    const API_KEY = '286c931aeec31c3b3c37ba892550098e'; 
+    axios.get(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+      .then(response => {
+        setAirQualityData(response.data.list[0]); // Set the first item in the list as air quality data
+      })
+      .catch(error => console.error('Error fetching the air quality data:', error));
+  };
+
+  const fetchWeatherData = (city) => {
+    const API_KEY = '286c931aeec31c3b3c37ba892550098e'; 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+      .then(response => response.json())
+      .then(data => {
+        setWeatherData(data);
+        console.log(JSON.stringify(data));
+        fetchAirQualityData(data.coord.lat, data.coord.lon); 
+        axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${API_KEY}`)
+        .then(response => {
+          setFiveDayForecast(response.data);
+        })
+        .catch(error => console.error('Error fetching the 5-day forecast data:', error));
+    
+      })
+      .catch(error => console.error('Error fetching the weather data:', error));
+  };
+
+  const handleSearch = (searchedCity) => {
+    setCity(searchedCity); 
+  };
+
+
+  return (
+    <div>
+      <Navbar onSearch={handleSearch} />
+      {weatherData && airQualityData &&  (
+        <div style={{ display: "flex", padding: "30px", gap: "20px" }}>
+          <div style={{ flex: "1", marginRight: "10px" }}>
+            <MainWeather weatherData={weatherData} />
+            <p style={{ fontWeight: "700", fontSize: "20px", marginTop: "20px", color:'white' }}>5 Days Forecast</p>
+            {fiveDayForecast && <FiveDayForecast forecastData={fiveDayForecast} />}
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", flex: "0.5", gap: "20px" }}>
+            <TodayHighlights weatherData={weatherData} airQualityData={airQualityData}  />
+            
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherDashboard;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import FilterDramaIcon from '@mui/icons-material/FilterDrama';
+import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import GpsFixedIcon from '@mui/icons-material/GpsFixed';
+
+const Navbar = ({ onSearch }) => {
+    const [searchCity, setsearchCity] = useState('')
+    const handleSearchclick = () => {
+        if (searchCity.trim()) {
+            onSearch(searchCity);
+        }
+    };
+
+    return (
+        <nav
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                marginTop: '10px',
+                padding: '10px',
+                paddingLeft: '30px',
+                paddingRight: '30px'
+            }}>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '2px', color:'white' }}>
+                <FilterDramaIcon />
+                <p style={{ fontWeight: 'bold', fontSize: '20px', color:'white' }}>Weather</p>
+            </div>
+
+            <div style={{ display: "flex", alignItems: 'center', gap: '4px' }}>
+                <TextField
+                    style={{ backgroundColor: 'white', borderRadius: '2rem', width: '22rem' }}
+                    placeholder="Search City"
+                    variant="outlined"
+                    value={searchCity}
+                    onChange={(e) => setsearchCity(e.target.value)}
+                />
+                <Button variant='contained'
+                    onClick={handleSearchclick}
+                    style={{ borderRadius: '6px', backgroundColor: '#4B5550' }}>
+                    Search
+                </Button>
+            </div>
+            <div
+                style={{
+                    marginTop: '1rem',
+                    fontSize: '16px',
+                    fontWeight: '700',
+                    backgroundColor: "#4B5550",
+                    height: '35px',
+                    width: '150px',
+                    color: 'white',
+                    gap: '2px',
+                    borderRadius: '6px',
+                    alignItems: 'center',
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}>
+                <GpsFixedIcon />
+                <p style={{ fontSize: "14px" }}>Current Location</p>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
